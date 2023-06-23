9cfe947ba8380b9eb261b810e5273f0491997cf4
Steve Odinkaru <54577873+SteveOdinkaru@users.noreply.github.com>
2023-06-23 19:00:21 +0100

    Add files via upload

---
+++ GeoTekAccelerometer.ino
@@ -0,0 +1,203 @@
+/*
+TITLE: GEOTEK HANDPUMP MONITORING SYSTEM USING MPU6050 ACCELEROMETER
+AUTHOR: ODINKARU STEVE FOR GEOTEK
+RF MODULE: ESPRESSIF SYSTEMS 
+*/
+
+#include <Arduino.h>
+#include <Adafruit_MPU6050.h>
+#include <Adafruit_Sensor.h>
+#include <Wire.h>
+#include <WiFi.h>
+#include <WiFiClientSecure.h>
+#include <HTTPClient.h>
+
+#define TOUCH_THRESHOLD 50 //Threshold for touch pin on ESP32 Board.
+
+Adafruit_MPU6050 mpu;
+
+const char* ssid     = "Steven";
+const char* password = "GitG12345";
+const char* serverName = "https://geotek.com.ng/accelerometer_post_data.php";
+String apiKeyValue = "tPmAT5Ab3j7F9";
+
+
+void callback(){}
+void initializeMPUandWiFi(){
+  Serial.println("Adafruit MPU6050 test!");
+
+  // Try to initialize!
+  if (!mpu.begin()) {
+    Serial.println("Failed to find MPU6050 chip");
+    while (1) {
+      delay(10);
+    }
+  }
+  Serial.println("MPU6050 Found!");
+
+  mpu.setAccelerometerRange(MPU6050_RANGE_8_G);
+  Serial.print("Accelerometer range set to: ");
+  switch (mpu.getAccelerometerRange()) {
+  case MPU6050_RANGE_2_G:
+    Serial.println("+-2G");
+    break;
+  case MPU6050_RANGE_4_G:
+    Serial.println("+-4G");
+    break;
+  case MPU6050_RANGE_8_G:
+    Serial.println("+-8G");
+    break;
+  case MPU6050_RANGE_16_G:
+    Serial.println("+-16G");
+    break;
+  }
+  mpu.setGyroRange(MPU6050_RANGE_500_DEG);
+  Serial.print("Gyro range set to: ");
+  switch (mpu.getGyroRange()) {
+  case MPU6050_RANGE_250_DEG:
+    Serial.println("+- 250 deg/s");
+    break;
+  case MPU6050_RANGE_500_DEG:
+    Serial.println("+- 500 deg/s");
+    break;
+  case MPU6050_RANGE_1000_DEG:
+    Serial.println("+- 1000 deg/s");
+    break;
+  case MPU6050_RANGE_2000_DEG:
+    Serial.println("+- 2000 deg/s");
+    break;
+  }
+
+  mpu.setFilterBandwidth(MPU6050_BAND_5_HZ);
+  Serial.print("Filter bandwidth set to: ");
+  switch (mpu.getFilterBandwidth()) {
+  case MPU6050_BAND_260_HZ:
+    Serial.println("260 Hz");
+    break;
+  case MPU6050_BAND_184_HZ:
+    Serial.println("184 Hz");
+    break;
+  case MPU6050_BAND_94_HZ:
+    Serial.println("94 Hz");
+    break;
+  case MPU6050_BAND_44_HZ:
+    Serial.println("44 Hz");
+    break;
+  case MPU6050_BAND_21_HZ:
+    Serial.println("21 Hz");
+    break;
+  case MPU6050_BAND_10_HZ:
+    Serial.println("10 Hz");
+    break;
+  case MPU6050_BAND_5_HZ:
+    Serial.println("5 Hz");
+    break;
+  }
+
+  delay(100);
+  
+  WiFi.begin(ssid, password);
+  Serial.println("Connecting");
+  while(WiFi.status() != WL_CONNECTED) { 
+    delay(500);
+    Serial.print(".");
+  }
+  Serial.println("");
+  Serial.print("Connected to WiFi network with IP Address: ");
+  Serial.println(WiFi.localIP());
+}
+void sendToCloud(){
+  /* Get new sensor events with the readings */
+  sensors_event_t a, g, temp;
+  mpu.getEvent(&a, &g, &temp);
+
+  /* Print out the values */
+  Serial.print("Acceleration X: ");
+  Serial.print(a.acceleration.x);
+  Serial.print(", Y: ");
+  Serial.print(a.acceleration.y);
+  Serial.print(", Z: ");
+  Serial.print(a.acceleration.z);
+  Serial.println(" m/s^2");
+
+  Serial.print("Rotation X: ");
+  Serial.print(g.gyro.x);
+  Serial.print(", Y: ");
+  Serial.print(g.gyro.y);
+  Serial.print(", Z: ");
+  Serial.print(g.gyro.z);
+  Serial.println(" rad/s");
+
+  Serial.print("Temperature: ");
+  Serial.print(temp.temperature);
+  Serial.println(" degC");
+
+  Serial.println("");
+
+  if(WiFi.status()== WL_CONNECTED){
+    WiFiClientSecure *client = new WiFiClientSecure;
+    client->setInsecure(); //don't use SSL certificate
+    HTTPClient https;
+    Serial.println("Successfully connected to Geotek.");
+    
+    //Domain name with URL path or IP address with path
+    https.begin(*client, serverName);
+    
+    // Specify content-type header
+    https.addHeader("Content-Type", "application/x-www-form-urlencoded");
+
+    //Prepare HTTP POST request data
+    String httpRequestData = "api_key=" + apiKeyValue + "&value1=" + String(a.acceleration.x)
+                           + "&value2=" + String(a.acceleration.y) + "&value3=" + String(a.acceleration.z) + "";
+
+    Serial.print("httpRequestData: ");
+    Serial.println(httpRequestData);
+
+    // Send HTTP POST request
+    int httpResponseCode = https.POST(httpRequestData);
+    
+    if (httpResponseCode>0) {
+      Serial.print("HTTP Response code: ");
+      Serial.println(httpResponseCode);
+    }
+    else {
+      Serial.print("Error code: ");
+      Serial.println(httpResponseCode);
+    }
+    // Free resources
+    https.end();
+  }
+  else {
+    Serial.println("WiFi Disconnected");
+  }
+  //send data every 1 second
+  delay(1000);
+}
+
+void setup() {
+  Serial.begin(115200);
+  while (!Serial)
+    delay(10);
+
+  initializeMPUandWiFi();
+
+  Serial.println("ESP32 has started");
+
+  touchAttachInterrupt(T0, callback, TOUCH_THRESHOLD);
+  esp_sleep_enable_touchpad_wakeup();
+
+  while(touchRead(T0)<50){
+    Serial.println("ESP32 is working...");
+    sendToCloud();
+    
+    if (touchRead(T0)>50){
+      delay(5000);
+      break;
+    }
+  }
+  Serial.println("Going to sleep now...");
+  esp_deep_sleep_start();
+}
+
+void loop(){
+}
